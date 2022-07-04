dbfe8e7d5270e18f64ea1d47d0b8a52bc50dbdb4
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-07-04 10:51:13 +0200

    snack 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿//SNACK 1
+
+//L’utente inserisce due numeri in successione.
+//Il software stampa il maggiore.
+
+int numA;
+int numB;
+
+Console.WriteLine("Inserisci un numero intero");
+numA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Inserisci un'altro numero intero");
+numB = Convert.ToInt32(Console.ReadLine());
+
+if(numA > numB)
+{
+    Console.WriteLine($"Il numero maggiore è: {numA}");
+}
+else
+{
+    Console.WriteLine($"Il numero maggiore è: {numB}");
+}
