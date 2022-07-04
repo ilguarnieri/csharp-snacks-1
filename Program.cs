973f132e19dab1dbd47291b13a6e9182604edf10
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-07-04 14:12:41 +0200

    snack 11

--- Program.cs
+++ Program.cs
@@ -1,13 +1,73 @@
-﻿//---------- SNACK 6 ----------
+﻿//---------- SNACK 12 ----------
+//Scrivere una funzione per verificare se un numero è pari o dispari.
+//Quindi chiedere un numero all'utente e comunicargli se è pari o dispari.
 {
-    Console.WriteLine("\n\n*** SNACK 6 *** ");
+    Console.WriteLine("*** SNACK 12 *** ");
 }
 
 
 
-//---------- SNACK 6 ----------
+//---------- SNACK 11 ----------
+//Dare la possibilità di inserire due parole.
+//Verificare tramite una funzione che le due parole abbiano la stessa
+//lunghezza. Se hanno la stessa lunghezza, stamparle entrambe,
+//altrimenti stampare la più lunga delle due.
 {
-    Console.WriteLine("\n\n*** SNACK 6 *** ");
+    Console.WriteLine("\n\n*** SNACK 11 *** ");
+
+    string GetWord()
+    {
+        Console.WriteLine("Scrivi una parola");
+        string word = Console.ReadLine();
+
+        return word;
+    }
+
+    void WordComparison(string word1, string word2)
+    {
+        if (word1.Length == word2.Length)
+        {
+            Console.WriteLine($"\n{word1}");
+            Console.WriteLine(word2);
+        } 
+        else if (word1.Length < word2.Length)
+        {
+            Console.WriteLine($"\n{word2}");
+        }
+        else
+        {
+            Console.WriteLine($"\n{word1}");
+        }
+    }
+
+    WordComparison(GetWord(), GetWord());
+}
+
+
+
+//---------- SNACK 10 ----------
+//Fai inserire un numero, che chiameremo N, all’utente.
+//Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100.
+//Ogni volta che ne crei uno, stampalo a schermo.
+{
+    Console.WriteLine("\n\n*** SNACK 10 *** ");
+    int n;
+
+    Console.WriteLine("Inserisci un numero");
+    n = Convert.ToInt32(Console.ReadLine());
+
+    for(int i = 0; i < n; i++)
+    {
+        Console.Write($"\nArray {i+1} [");
+        int[] array = new int[10];
+
+        for(int j = 0; j < array.Length; j++)
+        {
+            array[j] = new Random().Next(1, 101);
+            Console.Write($" {array[j]} ");
+        }
+        Console.Write("]");
+    }
 }
 
 
@@ -82,16 +142,15 @@
 //---------- SNACK 2 ----------
 //L’utente inserisce due parole in successione.
 //Il software stampa prima la parola più corta, poi la parola più lunga.
-
 {
     Console.WriteLine("\n\n*** SNACK 2 *** ");
     string primaParola;
     string secondaParola;
 
-    Console.WriteLine("Inserisci una parola");
+    Console.WriteLine("Scrivi una parola");
     primaParola = Console.ReadLine();
 
-    Console.WriteLine("Inserisci un'altra parola");
+    Console.WriteLine("Scrivi un'altra parola");
     secondaParola = Console.ReadLine();
 
     if (primaParola.Length > secondaParola.Length)
