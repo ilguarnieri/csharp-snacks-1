4fd6188cd6cea7781724c5ed7aab396fb606bbf2
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-07-04 11:02:03 +0200

    snack 2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿//SNACK 1
-
+﻿//---------- SNACK 1 ----------
 //L’utente inserisce due numeri in successione.
 //Il software stampa il maggiore.
 
@@ -14,9 +13,37 @@ numB = Convert.ToInt32(Console.ReadLine());
 
 if(numA > numB)
 {
-    Console.WriteLine($"Il numero maggiore è: {numA}");
+    Console.WriteLine($"\nIl numero più grande è: {numA}");
 }
 else
 {
-    Console.WriteLine($"Il numero maggiore è: {numB}");
+    Console.WriteLine($"\nIl numero più grande è: {numB}");
 }
+
+
+//---------- SNACK 2 ----------
+//L’utente inserisce due parole in successione.
+//Il software stampa prima la parola più corta, poi la parola più lunga.
+
+string primaParola;
+string secondaParola;
+
+Console.WriteLine("\n\nInserisci una parola");
+primaParola = Console.ReadLine();
+
+Console.WriteLine("Inserisci un'altra parola");
+secondaParola = Console.ReadLine();
+
+if (primaParola.Length > secondaParola.Length)
+{
+    Console.WriteLine($"\n{secondaParola}");
+    Console.WriteLine($"{primaParola}");
+}
+else
+{
+    Console.WriteLine($"\n{primaParola}");
+    Console.WriteLine($"{secondaParola}");
+}
+
+
+//---------- SNACK 3 ----------
